fcfda7da4a2b51fb0ae7d74f7a4d13ff8898edee
tony <tony@teamt5.org>
2022-09-07 23:51:44 +0800

    feat: add alert logo

---
+++ src/components/atoms/aAlertLogo/aAlertLogo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="a-alert-logo">
+    <img svg-inline src="../../../assets/alertLogoBorder.svg" alt="alert" />
+    <p>警</p>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.a-alert-logo {
+  position: relative;
+
+  svg {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  p {
+    font-family: 'Glow Sans TC', sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 38px;
+    line-height: 105px;
+    text-align: center;
+    color: #ccfdff;
+    margin: 0;
+  }
+}
+</style>
